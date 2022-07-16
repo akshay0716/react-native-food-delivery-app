e66cbea2715b9adce898c0c7a95c1e713ed29c89
akshay nagargoje <akshay.nagargoje@myrl.tech>
2022-07-16 16:57:39 +0530

    final code updated

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,10 +1,296 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import {
+  icons,
+  images,
+  COLORS,
+  FONTS,
+  SIZES,
+  GOOGLE_API_KEY,
+} from './../constants';
+import MapViewDirections from 'react-native-maps-directions';
 
-export default function OrderDelivery() {
+export default function OrderDelivery({route}) {
+  const [restaurant, setRestaurant] = useState(null);
+  const [streetName, setStreetName] = useState(null);
+  const [fromLocation, setFromLocation] = useState(null);
+  const [toLocation, setToLocation] = useState(null);
+  const [region, setRegion] = useState(null);
+
+  const mapView = useRef();
+
+  useEffect(() => {
+    const {restaurant, location} = route.params;
+
+    let fromLoc = location.gps;
+    let toLoc = restaurant.location;
+    let street = location.streetName;
+
+    let mapRegion = {
+      latitude: (fromLoc.latitude + toLoc.latitude) / 2,
+      longitude: (fromLoc.longitude + toLoc.longitude) / 2,
+      latitudeDelta: Math.abs(fromLoc.latitude - toLoc.latitude),
+      longitudeDelta: Math.abs(fromLoc.longitude - toLoc.longitude),
+    };
+
+    setRestaurant(restaurant);
+    setStreetName(street);
+    setFromLocation(fromLoc);
+    setToLocation(toLoc);
+    setRegion(mapRegion);
+  }, []);
+
+  const destinationMarker = () => {
+    return (
+      <Marker coordinate={toLocation}>
+        <View
+          style={{
+            height: 30,
+            width: 30,
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: COLORS.white,
+          }}>
+          <View
+            style={{
+              height: 30,
+              width: 30,
+              borderRadius: 20,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.primary,
+            }}>
+            <Image
+              source={icons.pin}
+              style={{width: 25, height: 25, tintColor: COLORS.white}}
+            />
+          </View>
+        </View>
+      </Marker>
+    );
+  };
+
+  const carIcon = () => {
+    return (
+      <Marker coordinate={fromLocation} anchor={{x: 0.5, y: 0.5}} flat={true}>
+        <Image source={icons.car} style={{width: 40, height: 40}} />
+      </Marker>
+    );
+  };
+
+  function renderMap() {
+    return (
+      <View style={{flex: 1}}>
+        <MapView
+          ref={mapView}
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          initialRegion={region}
+          style={{flex: 1}}>
+          <MapViewDirections
+            origin={fromLocation}
+            destination={toLocation}
+            apikey={'AIzaSyD_lMXvGFGsiO2qhvX7Vs49r-sdICRtrAQ'}
+          />
+          {destinationMarker()}
+          {carIcon()}
+        </MapView>
+      </View>
+    );
+  }
+
+  function renderDestinationHeader() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          top: 50,
+          left: 0,
+          right: 0,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: SIZES.width * 0.9,
+            paddingVertical: SIZES.padding,
+            paddingHorizontal: SIZES.padding * 2,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.white,
+            // justifyContent: 'center',
+          }}>
+          <Image
+            source={icons.red_pin}
+            style={{width: 30, height: 30, marginRight: SIZES.padding}}
+          />
+          <View style={{flex: 1}}>
+            <Text style={{...FONTS.body3, color: COLORS.black}}>
+              {streetName}
+            </Text>
+          </View>
+          <Text style={{...FONTS.body3, color: COLORS.black}}>10 min</Text>
+        </View>
+      </View>
+    );
+  }
+
+  function renderDeliveryInfo() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          alignItems: 'center',
+          justifyContent: 'center',
+          bottom: 50,
+          left: 0,
+          right: 0,
+        }}>
+        <View
+          style={{
+            width: SIZES.width * 0.9,
+            paddingVertical: SIZES.padding * 2,
+            paddingHorizontal: SIZES.padding * 3,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.white,
+          }}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            {/* avatar */}
+            <Image
+              source={images.avatar_1}
+              style={{width: 50, height: 50, borderRadius: 25}}
+            />
+            <View style={{flex: 1, marginLeft: SIZES.padding}}>
+              {/* name and rating */}
+              <View
+                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={{...FONTS.h4}}>{restaurant?.courier.name}</Text>
+                <View style={{flexDirection: 'row'}}>
+                  <Image
+                    source={icons.star}
+                    style={{width: 18, height: 18, tintColor: COLORS.primary}}
+                  />
+                  <Text style={{marginLeft: SIZES.padding, ...FONTS.body3}}>
+                    {restaurant?.rating}
+                  </Text>
+                </View>
+              </View>
+              <Text style={{color: COLORS.darkgray, ...FONTS.body4}}>
+                {' '}
+                {restaurant?.name}
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: SIZES.padding * 2,
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                marginRight: 10,
+                backgroundColor: COLORS.primary,
+                height: 50,
+                borderRadius: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text style={{color: COLORS.white, ...FONTS.body3}}>Call</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                backgroundColor: COLORS.primary,
+                height: 50,
+                borderRadius: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text style={{color: COLORS.white, ...FONTS.body3}}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  function zoomIn() {
+    let newRegion = {
+      latitude: region.latitude,
+      longitude: region.longitude,
+      latitudeDelta: region.latitudeDelta / 2,
+      longitudeDelta: region.longitudeDelta / 2,
+    };
+
+    setRegion(newRegion);
+    mapView.current.animateToRegion(newRegion, 200);
+  }
+
+  function zoomOut() {
+    let newRegion = {
+      latitude: region.latitude,
+      longitude: region.longitude,
+      latitudeDelta: region.latitudeDelta * 2,
+      longitudeDelta: region.longitudeDelta * 2,
+    };
+
+    setRegion(newRegion);
+    mapView.current.animateToRegion(newRegion, 200);
+  }
+
+  function renderButtons() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          bottom: SIZES.height * 0.35,
+          right: SIZES.padding * 2,
+          width: 60,
+          height: 130,
+          justifyContent: 'space-between',
+        }}>
+        {/* Zoom In */}
+        <TouchableOpacity
+          onPress={() => zoomIn()}
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+            backgroundColor: COLORS.white,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text style={{...FONTS.body1}}> + </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => zoomOut()}
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+            backgroundColor: COLORS.white,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text style={{...FONTS.body1}}> - </Text>
+        </TouchableOpacity>
+
+        {/* Zoom Out */}
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>OrderDelivery</Text>
+    <View style={{flex: 1}}>
+      {renderMap()}
+      {renderDestinationHeader()}
+      {renderDeliveryInfo()}
+      {renderButtons()}
     </View>
   );
 }
--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -11,6 +11,7 @@ import {icons, images, COLORS, FONTS, SIZES} from './../constants';
 import React, {useEffect, useState} from 'react';
 
 export default function Restaurant({navigation, route}) {
+  const scrollX = new Animated.Value(0);
   const [item, setRestaurant] = useState(null);
   const [location, setLocation] = useState(null);
 
@@ -18,7 +19,7 @@ export default function Restaurant({navigation, route}) {
     const {item, initialCurrentLocation} = route.params;
     console.log('item', item);
     setRestaurant(item);
-    setLocation(location);
+    setLocation(initialCurrentLocation);
   }, []);
 
   const renderHeader = () => {
@@ -73,7 +74,11 @@ export default function Restaurant({navigation, route}) {
         pagingEnabled
         scrollEventThrottle={5}
         snapToAlignment="center"
-        showsHorizontalScrollIndicator={true}>
+        showsHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {x: scrollX}}}],
+          {useNativeDriver: false},
+        )}>
         {item?.menu.map((item, index) => (
           <View key={`menu-${index}`} style={{alignItems: 'center'}}>
             {/* FOOD IMAGE */}
@@ -95,37 +100,77 @@ export default function Restaurant({navigation, route}) {
                   justifyContent: 'center',
                   flexDirection: 'row',
                 }}>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: COLORS.white,
-                    alignItems: 'center',
-                    width: 50,
-                    justifyContent: 'center',
-                    borderTopLeftRadius: 25,
-                    borderBottomLeftRadius: 25,
-                  }}>
-                  <Text style={{...FONTS.body1}}> - </Text>
-                </TouchableOpacity>
                 <View
                   style={{
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: 50,
                     backgroundColor: COLORS.white,
-                  }}>
-                  <Text style={{...FONTS.h2}}>5</Text>
-                </View>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: COLORS.white,
-                    alignItems: 'center',
-                    width: 50,
-                    justifyContent: 'center',
+                    flexDirection: 'row',
+                    borderTopLeftRadius: 25,
+                    borderBottomLeftRadius: 25,
                     borderTopRightRadius: 25,
                     borderBottomRightRadius: 25,
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}>
-                  <Text style={{...FONTS.body1}}> + </Text>
-                </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: COLORS.white,
+                      alignItems: 'center',
+                      width: 50,
+                      justifyContent: 'center',
+                      borderTopLeftRadius: 25,
+                      borderBottomLeftRadius: 25,
+                    }}>
+                    <Text style={{...FONTS.body1}}> - </Text>
+                  </TouchableOpacity>
+
+                  <View
+                    style={{
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      // width: 50,
+                      backgroundColor: COLORS.white,
+                    }}>
+                    <Text style={{...FONTS.h2}}>5</Text>
+                  </View>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: COLORS.white,
+                      alignItems: 'center',
+                      width: 50,
+                      justifyContent: 'center',
+                      borderTopRightRadius: 25,
+                      borderBottomRightRadius: 25,
+                    }}>
+                    <Text style={{...FONTS.body1}}> + </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              {/* name and description */}
+              <View
+                style={{
+                  width: SIZES.width,
+                  alignItems: 'center',
+                  marginTop: 30,
+                }}>
+                <Text style={{...FONTS.h2, color: COLORS.black}}>
+                  {item.name} - {item.price.toFixed(2)}
+                </Text>
+                <Text style={{...FONTS.body3}}>{item.description}</Text>
+              </View>
+              {/* calories */}
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginTop: 10,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Image
+                  source={icons.fire}
+                  style={{width: 20, height: 20, marginRight: 10}}></Image>
+                <Text style={{...FONTS.body3, color: COLORS.darkgray}}>
+                  {item.calories.toFixed(2)} Cal
+                </Text>
               </View>
             </View>
           </View>
@@ -134,10 +179,142 @@ export default function Restaurant({navigation, route}) {
     );
   }
 
+  function renderDots() {
+    const dotPossition = Animated.divide(scrollX, SIZES.width);
+
+    return (
+      <View style={{height: 30}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: SIZES.padding,
+          }}>
+          {item?.menu.map((item, index) => {
+            const opacity = dotPossition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: 'clamp',
+            });
+
+            const dotSize = dotPossition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
+              extrapolate: 'clamp',
+            });
+            const dotColor = dotPossition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
+              extrapolate: 'clamp',
+            });
+            return (
+              <Animated.View
+                key={`dot-${index}`}
+                opacity={opacity}
+                style={{
+                  borderRadius: SIZES.radius,
+                  marginHorizontal: 6,
+                  width: dotSize,
+                  height: dotSize,
+                  backgroundColor: dotColor,
+                }}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
+
+  function renderOrder() {
+    return (
+      <View>
+        {renderDots()}
+        <View
+          style={{
+            backgroundColor: COLORS.white,
+            borderTopLeftRadius: 40,
+            borderTopRightRadius: 40,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: SIZES.padding * 2,
+              paddingHorizontal: SIZES.padding * 3,
+              borderBottomColor: COLORS.lightGray2,
+              borderBottomWidth: 1,
+              alignItems: 'center',
+            }}>
+            <Text style={{...FONTS.h3, color: COLORS.black}}>
+              Items in cart
+            </Text>
+            <Text style={{...FONTS.h3, color: COLORS.black}}>$ 46.00</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              paddingVertical: SIZES.padding * 2,
+              paddingHorizontal: SIZES.padding * 3,
+            }}>
+            <View style={{flexDirection: 'row'}}>
+              <Image
+                source={icons.pin}
+                resizeMode="contain"
+                style={{
+                  width: 20,
+                  height: 20,
+                  tintColor: COLORS.darkgray,
+                }}></Image>
+              <Text style={{marginLeft: SIZES.padding, ...FONTS.h4}}>
+                Location
+              </Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Image
+                source={icons.master_card}
+                resizeMode="contain"
+                style={{width: 20, height: 20, tintColor: COLORS.darkgray}}
+              />
+              <Text style={{marginLeft: 10, ...FONTS.h4}}>8888</Text>
+            </View>
+          </View>
+          {/* order button */}
+          <View
+            style={{
+              padding: SIZES.padding,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('OrderDelivery', {
+                  restaurant: item,
+                  location,
+                })
+              }
+              style={{
+                width: SIZES.width * 0.9,
+                padding: SIZES.padding,
+                backgroundColor: COLORS.primary,
+                alignItems: 'center',
+                borderRadius: SIZES.radius,
+              }}>
+              <Text style={{color: COLORS.white, ...FONTS.h2}}>Order</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       {renderHeader()}
       {renderFoodInfo()}
+      {renderOrder()}
     </SafeAreaView>
   );
 }
